ccb125bbb8305ef53d45c458e3cac01e3f83208f
farhanalifviandi <130892023+farhanalifviandi@users.noreply.github.com>
2023-07-05 23:33:16 +0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// By Author 
+//   Farhan Alifviandi
+//     213510299
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+createApp(App).use(router).mount('#app')
